ee1a95bf9d31665562329b93870a821b5604390d
Arlindo Silva <arlindovicente@gmail.com>
2023-11-10 20:41:53 -0300

    tela de login
    frontend do login e do pagenotfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
+import { LoginComponent } from './modules/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: LoginComponent },
+  {path: '**', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { PrimengModule } from './primeng.module';
+import { PageNotFoundComponent } from './modules/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimengModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
