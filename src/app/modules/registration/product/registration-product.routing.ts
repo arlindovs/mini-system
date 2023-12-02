b8ff54d1e70134126838a2a7af0dcca7ebd1153d
Gabriel Pistilli <gabrielcarra.p@gmail.com>
2023-12-02 08:24:17 -0300

    Delete modulo registration product

--- src/app/modules/registration/product/registration-product.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import {RegistratioProductComponent} from "./page/registration-product/registration-product.component";
-
-export const registrationProductRoutes: Routes = [
-  {
-    path: '',
-    component: RegistratioProductComponent,
-  }
-]
