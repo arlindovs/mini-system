83121b013a10a0b1ec4a4693e2f3c94cb31f1bd8
Gabriel Pistilli <gabrielcarra.p@gmail.com>
2023-12-28 09:29:49 -0300

    up

--- src/app/models/interfaces/product/AddProductAction.ts
+++ src/app/models/interfaces/product/AddProductAction.ts
@@ -1,4 +1,5 @@
 export interface AddProductAction {
     action: string;
     id?: string;
+    productName?: string;
 }
---
+++ src/app/models/interfaces/user/event/EditUserAction.ts
@@ -0,0 +1,5 @@
+export interface EditUserAction {
+  action: string;
+  id?: string;
+  userName?: string;
+}
