5c5ca68065bb24fb129d13dc8d838cb9b3620229
Arlindo Silva <arlindovicente@gmail.com>
2023-12-29 17:32:53 -0300

    modulo usuario
    alteração geral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-  title = 'mini-system';
+  title = 'Minisys';
 
   constructor(private primeNgConfig: PrimeNGConfig) {}
 
--- src/app/modules/registration/user/registration-user.module.ts
+++ src/app/modules/registration/user/registration-user.module.ts
@@ -1,33 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegistrationUserComponent } from './page/registration-user/registration-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { PrimengModule } from 'src/app/libraries/primeng.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RegistrationUserRoutes } from './registration-user.routing';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
-import { UserTableComponent } from './components/user-table/user-table.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
 import { UserComponent } from './user/user.component';
-
-
+import { PrimengModule } from 'src/app/libraries/primeng.module';
 
 @NgModule({
-  declarations: [
-    RegistrationUserComponent,
-    UserTableComponent,
-    UserFormComponent,
-    UserComponent,
-  ],
-  imports: [
+    imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forChild(RegistrationUserRoutes),
     PrimengModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(RegistrationUserRoutes)
+  ],
+  declarations: [
+    UserComponent,
   ],
   providers: [MessageService,CookieService,ConfirmationService],
 })
